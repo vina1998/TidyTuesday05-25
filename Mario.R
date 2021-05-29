74f882e8b6eac5364aad97755989f0e50057e350
Thivina Thanabalan <thity006@mymail.unisa.edu.au>
2021-05-29 20:45:42 +0930

    Made some changes and added ggplot png

--- Mario.R
+++ Mario.R
@@ -3,41 +3,27 @@ drivers <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/ti
 
 library(tidyverse)
 library(ggimage)
-library(here)
 library(viridis)
 library(extrafont)
 
 loadfonts()
 
+#where is the line between analysis and visualizations lol
 
 drivers <- drivers %>% drop_na()
 
-invol_legal <- drivers %>%  
+#prep data
+df <- drivers %>%  
   count(year) %>% 
   rowwise() %>% 
   mutate(
     w = round(runif(1, 0, 3)),
-    h = runif(1, 0, 3), 
-    img = paste0("mario", round(runif(1, 1, 7)), ".png")) %>% 
+    h = runif(1, 0, 3)) %>% 
   ungroup()
 
-
-
-ggplot(invol_legal) + geom_curve(aes(x = -8.25 - w, y = year - w * 0.4, xend = n, yend = 1997 + h, color = year, size = n), curvature =-1)  + scale_color_gradientn(colours=rainbow(5))  +theme_void() + theme(plot.background = element_rect(fill = "black", colour=NA)) 
-+   scale_size_continuous(range = c(0.1, 0.5)) +   scale_x_continuous(breaks = 0:20, labels = ifelse(0:20 %% 5 == 0, 0:20, ""), limits = c(-20, 23)) +  scale_y_continuous(limits = c(1997, 2021))+
-  theme(
-    legend.position = "none",
-    axis.ticks.x = element_line(color = "white"),
-    axis.ticks.length.x = unit(0.5, "line", "white"),
-    axis.ticks.y = element_line(color = "white"),
-    axis.ticks.length.y = unit(0.5, "line", "white"),
-    axis.text.x = element_text(colour="white"),
-    axis.text.y= element_text(colour="white"))
-
-
-#where is the line between analysis and visualisations
-windows <- data.frame(
-  fyear = invol_legal$year
+#starting line
+starting_line <- data.frame(
+  fyear = df$year
 ) %>%
   mutate(
     r = row_number(),
@@ -52,7 +38,7 @@ windows <- data.frame(
   unnest(c(x, y))
 
 #track:
-ggplot(invol_legal) + 
+ggplot(df) + 
   annotate("polygon", x = c(-20, 15, 10, -20), y = c(1997, 1997, 2021, 2021), fill = "grey30", color = "grey30") + 
   annotate("polygon", x = c(15, 22.5, 22.5, 10), y = c(1997, 1997, 2021, 2021), fill = "grey30", color = "grey30") + 
   annotate("polygon", x = c(-20, 15, 10, -20), y = c(2020.3, 2020.3, 2030, 2030), fill = "palegreen3", color = "palegreen3") + 
@@ -67,7 +53,7 @@ ggplot(invol_legal) +
   annotate("line", x = c(10,12), y=2010, colour="white", size=2) + 
   annotate("line", x = c(14,16), y=2010, colour="white", size=2) +
   annotate("line", x = c(18,20), y=2010, colour="white", size=2) + 
-  geom_polygon(data = windows, aes(x = x - 21, y = y - 1.9, group = interaction(c, r)), fill = "white", color = "black", size= 1.5) +  
+  geom_polygon(data = starting_line, aes(x = x - 21, y = y - 1.9, group = interaction(c, r)), fill = "white", color = "black", size= 1.5) +  
   annotate("polygon", x = c(-20, 15, 10, -20), y = c(1987.3, 1987.3, 1997, 1997), fill = "palegreen3", color = "palegreen3") + 
   annotate("polygon", x = c(15, 22.5, 22.5, 10), y = c(1987.3, 1987.3, 1997, 1997), fill = "palegreen3", color = "palegreen3") +  
   geom_curve(aes(x = -15 - w, y = year - w * 0.4, xend = n, yend = 2001 + h, color = year, size = n), curvature = 0) +   
@@ -92,7 +78,10 @@ ggplot(invol_legal) +
   annotate("text", x = 19, y = 1994, hjust = 1, vjust = 1, label = "?", size = 15, color = "seashell3", family= "Impact") +
   annotate("text", x = 16, y = 2026, hjust = 1, vjust = 1, label = "?", size = 15, color = "white", family= "Impact") + 
   annotate("text", x = 19, y = 2027, hjust = 1, vjust = 1, label = "?", size = 15, color = "seashell3", family= "Impact") + 
-  annotate("text", x = 10, y = 2024, hjust = 1, vjust = 1, label = "Number of records by year", size = 10, family= "Comic Sans MS",color = "white") + 
+  annotate("text", x = 10, y = 2025, hjust = 1, vjust = 1, label = "Number of records by year", size = 10, family= "Comic Sans MS",color = "white") + 
+  annotate("text", x = 17.5, y = 2022, hjust = 1, vjust = 1, label = "Interesting observation:overlap in number of records obtained between earliest and later years", size= 5, family= "Comic Sans MS",color = "black") + 
+  annotate("text", x = 17.5, y = 2019, hjust = 1, vjust = 1, label = "Source: Mario Kart World Records, Graphics: Thivina Thanabalan", size= 5, family= "Comic Sans MS",color = "grey48") + 
+  
   scale_x_continuous(breaks = 0:20, labels = ifelse(0:20 %% 5 == 0, 0:20, ""), limits = c(-20, 23)) + 
   theme_void() + 
   theme(legend.position = "none",axis.ticks.y = element_line(color = "yellow"), 
@@ -103,4 +92,6 @@ ggplot(invol_legal) +
         axis.text.x = element_text(colour="lemonchiffon4")) + 
   theme(plot.background = element_rect(fill = "lemonchiffon", colour=NA)) +   
   geom_image(aes(x = n, y = 2002 + h - 0.5, image = "https://www.pngkey.com/png/full/1007-10074038_bananas-transparent-mario-kart.png"), size = 0.035, by = "height") + 
-  geom_image(aes(x = 1, y = 2027, image = "https://www.pngkey.com/png/full/140-1403727_super-mario-kart-png-file-mario-kart-super.png"), size = 0.1, by = "height")
+  geom_image(aes(x = 1, y = 2027, image = "https://www.pngkey.com/png/full/140-1403727_super-mario-kart-png-file-mario-kart-super.png"), size = 0.1, by = "height") + 
+  ggsave(path = "TidyTuesday05-25", filename = "track.png")
+
